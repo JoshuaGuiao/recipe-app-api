73feb614ac4fbd2f0a566910545a77bce498da3c
joshguiao <djosh.guiao@gmail.com>
2024-07-15 17:23:51 +0800

    Clear lints

--- app/app/tests.py
+++ app/app/tests.py
@@ -11,11 +11,11 @@ class CalcTest(SimpleTestCase):
 
     def test_add_number(self):
         """ adding number together"""
-        res = calc.add(5,6)
+        res = calc.add( 5 , 6 )
 
-        self.assertEqual(res, 11)
+        self.assertEqual( res , 11 )
 
     def test_subtract_number(self):
         """ subtracting number together"""
-        res = calc.subtract(6,5)
-        self.assertEqual(res, 1)
+        res = calc.subtract( 6 , 5 )
+        self.assertEqual(res , 1)
