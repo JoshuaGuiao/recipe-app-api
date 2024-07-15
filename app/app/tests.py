5de64d614b094e5280afdb8947b68ba00fdbef28
joshguiao <djosh.guiao@gmail.com>
2024-07-15 17:20:32 +0800

    Added Dependencies for postgres

---
+++ app/app/tests.py
@@ -0,0 +1,21 @@
+"""
+Sample Test
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTest(SimpleTestCase):
+    """ Test the calc method"""
+
+    def test_add_number(self):
+        """ adding number together"""
+        res = calc.add(5,6)
+
+        self.assertEqual(res, 11)
+
+    def test_subtract_number(self):
+        """ subtracting number together"""
+        res = calc.subtract(6,5)
+        self.assertEqual(res, 1)
