77c0b6da265e8988798731b485fca17597300264
joshguiao <djosh.guiao@gmail.com>
2024-08-05 11:34:57 +0800

    Configured

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,12 +1,12 @@
-"""
+""" #noqa
 Calculator Functions
 
 """
 
-def add( x , y ):
+def add(x,y) : #noqa
     """Add x and y and return result"""
     return x+y
 
-def subtract( x , y ):
+def subtract( x ,y ) :
     """Add x and y and return result"""
     return x-y
